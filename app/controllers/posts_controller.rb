3a6a4edfff3a93cb5f70a4f95686053e1ee14ab0
Gao Ciaorun <itchynozz@gmail.com>
2020-07-09 02:18:44 +0800

    貼文邊界fixed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,9 +3,14 @@ class PostsController < ApplicationController
     @posts = Post.all.order('created_at DESC')
   end
 
-  def new 
+  def new
+    @post = Post.new 
   end
-
+  
+  def show
+    @post = Post.find(params[:id])
+  end
+  
   def create 
     @post = Post.new(post_params)
     @post.save
@@ -13,9 +18,6 @@ class PostsController < ApplicationController
     redirect_to @post
   end
 
-  def show
-    @post = Post.find(params[:id])
-  end
 
   private 
   def post_params
