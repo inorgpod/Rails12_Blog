1f373d22aaf9fe84075ec43349a9903fade3330e
Gao Ciaorun <itchynozz@gmail.com>
2020-07-09 02:53:14 +0800

    edit/update button added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,19 +7,36 @@ class PostsController < ApplicationController
     @post = Post.new 
   end
   
-  def show
-    @post = Post.find(params[:id])
-  end
-  
   def create 
     @post = Post.new(post_params)
     
     if @post.save
-        redirect_to @post
+      redirect_to @post
     else 
-      render 'new'
+      render 'new' #如果沒有存取成功,會渲染一樣的new畫面，這樣頁面的資料不會被洗掉導致需要重新輸入
     end
   end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+  
+
+  def edit 
+    @post = Post.find(params[:id])
+  end
+
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(params[:post].permit(:title, :body))
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
 
 
   private 
